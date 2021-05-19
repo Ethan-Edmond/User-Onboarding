06a54f2cf6a7b6c6b414d8fc4e56b9215bda98f5
Ethan-Edmond <eedmond@uccs.edu>
2021-05-19 13:28:44 -0600

    Got the form working, needs validation and styling

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,14 +1,46 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import './App.css';
 import Form from "./components/Form";
 
+const initFormValues = {
+  username: "",
+  email: "",
+  password: "",
+  terms: false,
+};
 
 function App() {
-  const [formValues, setFormValues] = useState(null);
+  const [formValues, setFormValues] = useState(initFormValues);
+  const [users, setUsers] = useState([]);
+
+  const formUpdate = (name, value) => {
+    setFormValues({...formValues, [name]: value});
+  };
+
+  const formSubmit = () => {
+    setUsers([...users, {
+      username: formValues.username.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
+      terms: formValues.terms
+    }]);
+    setFormValues(initFormValues);
+  };
+
   return (
-    <>
-      <Form values={formValues}/>
-    </>
+    <div className="App">
+      <Form values={formValues} update={formUpdate} submit={formSubmit}/>
+      {users.map(user => {
+        return (
+          <div className="user">
+            <h1>{user.username}</h1>
+            <p>{user.email}</p>
+            <p>{user.password}</p>
+            <p>{user.terms ? "Has agreed to the terms" : "Has not agreed to the terms"}</p>
+          </div>
+        );
+      })}
+    </div>
   );
 }
 
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,6 +1,34 @@
+import React from "react";
 
-function Form(props){
-  return "AAA";
+function Form({values, update, submit}){
+
+  const onChange = (event) => {
+    const valueToUse = (event.target.type === "checkbox") ? event.target.checked : event.target.value;
+    update(event.target.name, valueToUse);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    submit();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <label>UserName
+        <input onChange={onChange} value={values.username} type="text" name="username"/>
+      </label>
+      <label>Email
+      <input onChange={onChange} value={values.email} type="email" name="email"/>
+      </label>
+      <label>Password
+      <input onChange={onChange} value={values.password} type="text" name="password"/>
+      </label>
+      <label>Do you agree to the terms?
+      <input onChange={onChange} checked={values.terms} type="checkbox" name="terms"/>
+      </label>
+      <button>Submit</button>
+    </form>
+  );
 }
 
 export default Form;

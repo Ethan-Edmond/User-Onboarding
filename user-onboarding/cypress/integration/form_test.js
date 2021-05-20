01ddad6365a6db11f08f2a9895af267d754cb35d
Ethan-Edmond <eedmond@uccs.edu>
2021-05-20 14:17:06 -0600

    Added tests for empty username and email

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -1,3 +1,18 @@
+function username(){
+  return cy.get("input[name=username]");
+}
+function email(){
+  return cy.get("input[name=email]");
+}
+function password(){
+  return cy.get("input[name=password]");
+}
+function terms(){
+  return cy.get("input[name=terms]");
+}
+function submit(){
+  return cy.contains("Submit");
+}
 
 describe("Cypress and url checking", () => {
   it("Sanity checks for cypress", () => {
@@ -10,19 +25,31 @@ describe("Cypress and url checking", () => {
 
 describe("MVP tests", () => {
   it("Gets the username input, then types a name into it", () => {
-    cy.get("input[name=username]").type("Ethan").should("have.value", "Ethan");
+    username().type("Ethan").should("have.value", "Ethan");
   });
   it("Gets the email input, then types an email into it", () => {
-    cy.get("input[name=email]").type("ethan@fakemail.com").should("have.value", "ethan@fakemail.com");
+    email().type("ethan@fakemail.com").should("have.value", "ethan@fakemail.com");
   });
   it("Gets the password input, then types a password into it", () => {
-    cy.get("input[name=password]").type("asdfjdlvklad").should("have.value", "asdfjdlvklad");
+    password().type("asdfjdlvklad").should("have.value", "asdfjdlvklad");
   });
   it("Clicks the checkbox", () => {
-    cy.get("input[name=terms]").click().should("be.checked");
+    terms().click().should("be.checked");
   });
   it("Submits the form", () => {
-    cy.contains("Submit").click();
+    submit().click();
     cy.contains("User: Ethan").should("exist");
+  });
+  describe("Checks that the submit button is disabled if one of the inputs is left empty", () => {
+    it("username left empty", () => {
+      email().type("a@a.com");
+      password().type("vcekld");
+      submit().should("be.disabled");
+    });
+    it("email left empty", () => {
+      email().clear();
+      username().type("dfjadsf");
+      submit().should("be.disabled");
+    });
   });
 });

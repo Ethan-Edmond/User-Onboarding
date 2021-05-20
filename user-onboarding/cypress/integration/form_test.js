4b4019ef24ab78cbf110f229ad0c49e1e36acb3c
Ethan-Edmond <eedmond@uccs.edu>
2021-05-20 13:48:24 -0600

    Installed cypress and added some basic tests

---
+++ user-onboarding/cypress/integration/form_test.js
@@ -0,0 +1,12 @@
+
+describe("Cypress and url checking", () => {
+  it("Sanity checks for cypress", () => {
+    expect(true).to.equal(true);
+  });
+  it("Visits url", () => {
+    cy.visit('');
+  });
+});
+
+describe("MVP tests", () => {
+});
